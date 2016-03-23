4d2132ba202ef9d217cf397b358fb72a2bd65286
Dave MacDonald <mindlapse@gmail.com>
2016-03-23 19:54:09 +0000

    Moved the wiremock terraform module to https://github.com/mindlapse/terraform-wiremock

--- terraform.tf
+++ terraform.tf
@@ -4,11 +4,11 @@ variable "aws_ssh_key_name" {}
 variable "aws_ssh_key_path" {}
 
 module "wiremock" {
-  source = "./modules/wiremock"
+  source = "github.com/mindlapse/terraform-wiremock.git"
   repo = "https://github.com/mindlapse/redesigned-pancake.git"
   branch = "master"
-  secret_key = "${aws_secret_key}"
-  access_key = "${aws_access_key}"
-  ssh_key_name = "${aws_ssh_key_name}"
-  ssh_key_path = "${aws_ssh_key_path}"
+  aws_secret_key   = "${aws_secret_key}"
+  aws_access_key   = "${aws_access_key}"
+  aws_ssh_key_name = "${aws_ssh_key_name}"
+  aws_ssh_key_path = "${aws_ssh_key_path}"
 }
