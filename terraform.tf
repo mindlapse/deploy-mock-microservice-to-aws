b5ea18785bd7e0cd16eacce81cce4389c28c9c39
Dave MacDonald <mindlapse@gmail.com>
2016-03-23 16:17:20 -0400

    Updated readme

--- terraform.tf
+++ terraform.tf
@@ -7,8 +7,8 @@ module "wiremock" {
   source = "github.com/mindlapse/terraform-wiremock.git"
   repo = "https://github.com/mindlapse/redesigned-pancake.git"
   branch = "master"
-  aws_secret_key   = "${aws_secret_key}"
-  aws_access_key   = "${aws_access_key}"
-  aws_ssh_key_name = "${aws_ssh_key_name}"
-  aws_ssh_key_path = "${aws_ssh_key_path}"
+  aws_secret_key   = "${var.aws_secret_key}"
+  aws_access_key   = "${var.aws_access_key}"
+  aws_ssh_key_name = "${var.aws_ssh_key_name}"
+  aws_ssh_key_path = "${var.aws_ssh_key_path}"
 }
